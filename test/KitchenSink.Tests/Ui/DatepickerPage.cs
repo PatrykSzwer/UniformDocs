0dac91d5b9c62eb5dd0e47aad8a7e3be27e70b9b
Patryk Szwermer <patryk.szwer@gmail.com>
2018-01-23 14:43:14 +0100

    Fixed tests due to new date picker element

--- test/KitchenSink.Tests/Ui/DatepickerPage.cs
+++ test/KitchenSink.Tests/Ui/DatepickerPage.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using System.Linq;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
 
 namespace KitchenSink.Tests.Ui
@@ -10,7 +11,7 @@ namespace KitchenSink.Tests.Ui
             PageFactory.InitElements(Driver, this);
         }
 
-        [FindsBy(How = How.CssSelector, Using = ".kitchensink-test-pikaday-input")]
+        [FindsBy(How = How.TagName, Using = "vaadin-date-picker")]
         public IWebElement DatePicker { get; set; }
 
         [FindsBy(How = How.XPath, Using = "//div[@class = 'pika-lendar']//table//tbody//td[@class = 'is-selected']//button[@class = 'pika-button pika-day']")]
@@ -27,9 +28,12 @@ namespace KitchenSink.Tests.Ui
 
         public void SelectDate(string date)
         {
-            DatePicker.Clear();
-            DatePicker.SendKeys(date);
-            DatePicker.SendKeys(Keys.Enter);
+            DatePicker = Driver.FindElement(By.TagName("vaadin-date-picker")); // Has to be found again due to incorrect type - EventFiringWebElement of current DatePicker object
+            var vaadinTextFieldElement = GetShadowElementByQuerySelector(DatePicker, "vaadin-text-field");
+            var pickerInput = GetShadowElementByQuerySelector(vaadinTextFieldElement, "input[autocomplete=\"off\"]");
+            pickerInput.Clear();
+            pickerInput.SendKeys(date);
+            pickerInput.SendKeys(Keys.Enter);
         }
     }
 }
