03834ec1fedf24b8b850931af66ce3304f0acc80
Egor Krushinov <egor.krushinov@starcounter.com>
2018-01-31 16:20:20 +0300

    Problem: two test-specific CSS-classes were left in AutoCompletePage.
    Solution: that classes were replaced to XPath selectors.

--- test/KitchenSink.Tests/Ui/AutoCompletePage.cs
+++ test/KitchenSink.Tests/Ui/AutoCompletePage.cs
@@ -18,10 +18,10 @@ namespace KitchenSink.Tests.Ui
         [FindsBy(How = How.XPath, Using = "//input[@slot = 'kitchensink/autocomplete-places-input']")]
         public IWebElement PlaceInput { get; set; }
 
-        [FindsBy(How = How.ClassName, Using = "kitchensink-test-autocomplete-products-item")]
+        [FindsBy(How = How.XPath, Using = "//ul[@slot = 'kitchensink/autocomplete-products-autocomplete']/template/li")]
         public IList<IWebElement> ProductsAutoComplete { get; set; }
 
-        [FindsBy(How = How.ClassName, Using = "kitchensink-test-autocomplete-places-item")]
+        [FindsBy(How = How.XPath, Using = "//ul[@slot = 'kitchensink/autocomplete-places-autocomplete']/template/li")]
         public IList<IWebElement> PlacesAutoComplete { get; set; }
 
         [FindsBy(How = How.XPath, Using = "//p[@slot = 'kitchensink/autocomplete-places-capital']")]
