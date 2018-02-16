274e319a15c7c3252df0cd8d19c46ba942eaee3b
Egor Krushinov <egor.krushinov@starcounter.com>
2018-02-16 17:06:40 +0300

    Problem: several more test-specific CSS-classes were found and should be removed.
    Solution: mentioned test-specific CSS-classes were removed, CssSelector's now use slot attributes.

--- test/KitchenSink.Tests/Ui/TablePage.cs
+++ test/KitchenSink.Tests/Ui/TablePage.cs
@@ -14,10 +14,10 @@ namespace KitchenSink.Tests.Ui
         [FindsBy(How = How.XPath, Using = "//button[text() = 'Add a pet']")]
         public IWebElement AddPetButton { get; set; }
 
-        [FindsBy(How = How.XPath, Using = "//table[@class='table table-striped']")]
+        [FindsBy(How = How.CssSelector, Using = "[slot = 'kitchensink/table-table']")]
         public IWebElement PetsTable { get; set; }
 
-        [FindsBy(How = How.XPath, Using = "//table[@class='table table-striped']//tbody//tr")]
+        [FindsBy(How = How.CssSelector, Using = "[slot = 'kitchensink/table-table'] tbody tr")]
         public IList<IWebElement> PetsTableRows { get; set; }
 
         public void AddPet()
