47b185049cbffc2cbd4a11235dd3dc06ccf324e2
PatrykSzwer <patryk.szwer@gmail.com>
2018-06-15 16:18:24 +0200

    Heavy improvements to server side implementation of DataTablePage - reimplemented GetPage method, Init method, small code improvements.
    Renamed Size to RowsCount for clearness, removed Header from Columns viewmodel, added Path and flag about sorting allowence

--- src/KitchenSink/ViewModels/Design/DataTablePage.json.cs
+++ src/KitchenSink/ViewModels/Design/DataTablePage.json.cs
@@ -8,40 +8,18 @@ namespace KitchenSink.ViewModels.Design
 {
     partial class DataTablePage : Json
     {
-        private List<string> ColumnHeaders => new List<string> {"First Name", "Last Name", "Email"};
-
         public IEnumerable<TableRow> Rows { get; set; }
-        public int PageSize = 25;
-        public int Size;
+        public int RowsCount => DbLinq.Objects<TableRow>().Count();
+        public int PageSize => 100;
 
         public void Init()
         {
-            this.PopulateColumns();
-            this.GetPage(0);
-            this.Size = DbLinq.Objects<TableRow>().Count();
+            this.Rows = DbLinq.Objects<TableRow>().Take(PageSize);
         }
 
         private void GetPage(int page)
         {
-            var offset = page * PageSize;
-            var PageRows = DbLinq.Objects<TableRow>().Skip(offset).Take(PageSize);
-            if (Rows == null)
-            {
-                this.Rows = PageRows;
-            }
-            else
-            {
-                this.Rows = Rows.Take(offset).Concat(PageRows).Concat(Rows.Skip(offset + PageSize));
-            }
-        }
-
-        private void PopulateColumns()
-        {
-            foreach (var columnHeader in ColumnHeaders)
-            {
-                var column = this.Columns.Add();
-                column.Header = columnHeader;
-            }
+            this.Rows = Rows.Concat(DbLinq.Objects<TableRow>().Skip(page * PageSize).Take(PageSize));
         }
 
         void Handle(Input.AddNewRowTrigger action)
@@ -55,11 +33,6 @@ namespace KitchenSink.ViewModels.Design
                     Email = "New email"
                 };
             });
-
-            this.Size = DbLinq.Objects<TableRow>().Count();
-
-            // TODO: update all pages instead of only the first one?
-            this.GetPage(0);
         }
 
         void Handle(Input.Page action)
@@ -72,10 +45,7 @@ namespace KitchenSink.ViewModels.Design
         {
             void Handle(Input.DeleteTrigger action)
             {
-                // TODO: Delete the row from the DB
                 this.Data.Delete();
-                
-                ((DataTablePage)Parent.Parent).Size = DbLinq.Objects<TableRow>().Count();
             }
         }
     }
