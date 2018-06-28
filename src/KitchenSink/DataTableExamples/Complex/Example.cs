dc4840e8c1ffbe0adf4e753ba2dda6e0e2187e7e
Marcin Goliński <mj.golinski@gmail.com>
2018-06-28 21:27:16 +0200

    SCRAPTHAT Example of using new DataTable Builder

---
+++ src/KitchenSink/DataTableExamples/Complex/Example.cs
@@ -0,0 +1,40 @@
+﻿using KitchenSink.Database;
+using Starcounter;
+using Starcounter.Linq;
+using Starcounter.Uniform.Builder;
+
+namespace KitchenSink.DataTableExamples.Complex
+{
+    namespace Complex
+    {
+
+        public class Example
+        {
+            public Json DoExample()
+            {
+                return new DataTableBuilder<Book, BookViewModel>(DbLinq.Objects<Book>())
+                    .AddColumn(b => b.Position, isSortable: true, isFilterable: true, displayName: "no.")
+                    .AddColumn(b => b.Display, isSortable: true, isFilterable: true, displayName: "Author&Title")
+                    .SetConverter(CreateBookViewModel)
+                    .SetSorterFilter(new BookSorterFilter())
+                    .Build();
+            }
+
+            private BookViewModel CreateBookViewModel(Book book)
+            {
+                var bookViewModel = new BookViewModel()
+                {
+                    DeleteAction = DeleteBook 
+                };
+                ((Json) bookViewModel).Data = book;
+                return bookViewModel;
+            }
+
+            private void DeleteBook(BookViewModel bookViewModel)
+            {
+                bookViewModel.Data.Delete();
+                // somehow notify that data source has changed
+            }
+        }
+    }
+}
---
+++ src/KitchenSink/DataTableExamples/Simple/Example.cs
@@ -0,0 +1,19 @@
+﻿using KitchenSink.Database;
+using Starcounter;
+using Starcounter.Linq;
+using Starcounter.Uniform.Builder;
+
+namespace KitchenSink.DataTableExamples.Simple
+{
+    public class Example
+    {
+        public Json DoExample()
+        {
+            return new DataTableBuilder<Book, BookViewModel>(DbLinq.Objects<Book>())
+                .AddColumn(b => b.Position, isSortable: true, isFilterable: true, displayName: "no.")
+                .AddColumn(b => b.Title, isSortable: true, isFilterable: true, displayName: "Title")
+                .AddColumn(b => b.Author, isSortable: true, isFilterable: true, displayName: "Author")
+                .Build();
+        }
+    }
+}
