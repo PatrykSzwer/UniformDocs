3780b95ab4b1499ad54c17eec92c85e87c69425d
Omar Alshaker <omar@omaralshaker.com>
2018-07-05 15:50:12 +0200

    Move uni-data-table demo

---
+++ src/UniformDocs/ViewModels/Components/DatatablePage.json.cs
@@ -0,0 +1,237 @@
+using Starcounter;
+
+namespace UniformDocs.ViewModels.Components
+{
+    partial class DatatablePage : Json
+    {
+
+        protected override void OnData()
+        {
+            base.OnData();
+
+            var rows = this.People.Add();
+
+            var person = rows.Rows.Add();
+            person.Name = "Dolores";
+            person.LastName = "Witt";
+            person.Age = 31;
+            person.EyeColor = "brown";
+               
+            person = rows.Rows.Add();
+            person.Name = "Griffith";
+            person.LastName = "Whitley";
+            person.Age = 21;
+            person.EyeColor = "blue";
+            
+   
+            person = rows.Rows.Add();
+            person.Name = "Verna";
+            person.LastName = "Galloway";
+            person.Age = 23;
+            person.EyeColor = "green";
+            
+   
+            person = rows.Rows.Add();
+            person.Name = "Conner";
+            person.LastName = "Sheppard";
+            person.Age = 21;
+            person.EyeColor = "blue";
+            
+   
+            person = rows.Rows.Add();
+            person.Name = "Isabelle";
+            person.LastName = "Wilcox";
+            person.Age = 22;
+            person.EyeColor = "browsn";
+            
+   
+            person = rows.Rows.Add();
+            person.Name = "Floyd";
+            person.LastName = "Blackburn";
+            person.Age = 33;
+            person.EyeColor = "blue";
+            
+   
+            person = rows.Rows.Add();
+            person.Name = "Rosalyn";
+            person.LastName = "Compton";
+            person.Age = 36;
+            person.EyeColor = "green";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Carmella";
+            person.LastName = "Solis";
+            person.Age = 33;
+            person.EyeColor = "browsn";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Isabella";
+            person.LastName = "Dillard";
+            person.Age = 26;
+            person.EyeColor = "blue";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Luella";
+            person.LastName = "Byrd";
+            person.Age = 39;
+            person.EyeColor = "green";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Blevins";
+            person.LastName = "Glenn";
+            person.Age = 31;
+            person.EyeColor = "browsn";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Lidia";
+            person.LastName = "Gibson";
+            person.Age = 21;
+            person.EyeColor = "blue";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Bright";
+            person.LastName = "Merrill";
+            person.Age = 21;
+            person.EyeColor = "blue";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Colleen";
+            person.LastName = "Roy";
+            person.Age = 27;
+            person.EyeColor = "blue";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Madden";
+            person.LastName = "Bowen";
+            person.Age = 30;
+            person.EyeColor = "blue";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Gay";
+            person.LastName = "Cabrera";
+            person.Age = 20;
+            person.EyeColor = "browsn";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Moran";
+            person.LastName = "Richard";
+            person.Age = 27;
+            person.EyeColor = "green";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Luna";
+            person.LastName = "Ramos";
+            person.Age = 36;
+            person.EyeColor = "green";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Pierce";
+            person.LastName = "rowse";
+            person.Age = 22;
+            person.EyeColor = "blue";
+            
+   
+            person = rows.Rows.Add();
+            person.Name = "Bobbie";
+            person.LastName = "Woodard";
+            person.Age = 20;
+            person.EyeColor = "green";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Lolita";
+            person.LastName = "Fry";
+            person.Age = 36;
+            person.EyeColor = "green";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Gertrude";
+            person.LastName = "Joyce";
+            person.Age = 30;
+            person.EyeColor = "blue";
+            
+   
+            person = rows.Rows.Add();
+            person.Name = "Rosa";
+            person.LastName = "Craft";
+            person.Age = 35;
+            person.EyeColor = "browsn";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Norris";
+            person.LastName = "Medina";
+            person.Age = 22;
+            person.EyeColor = "blue";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Beverley";
+            person.LastName = "Gilbert";
+            person.Age = 39;
+            person.EyeColor = "green";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Dionne";
+            person.LastName = "Riggs";
+            person.Age = 37;
+            person.EyeColor = "green";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Acevedo";
+            person.LastName = "Warner";
+            person.Age = 30;
+            person.EyeColor = "blue";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Ollie";
+            person.LastName = "Miranda";
+            person.Age = 31;
+            person.EyeColor = "green";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Clare";
+            person.LastName = "Blair";
+            person.Age = 25;
+            person.EyeColor = "blue";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Simmons";
+            person.LastName = "Espinoza";
+            person.Age = 25;
+            person.EyeColor = "blue";
+
+   
+            person = rows.Rows.Add();
+            person.Name = "Sloan";
+            person.LastName = "Callahan";
+            person.Age = 23;
+            person.EyeColor = "blue";
+            
+   
+            person = rows.Rows.Add();
+            person.Name = "Eva";
+            person.LastName = "Cherry";
+            person.Age = 20;
+            person.EyeColor = "green";
+        }
+    }
+}
