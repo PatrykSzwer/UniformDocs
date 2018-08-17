27a52faabb8df0de9e222803348fb360afbe72a3
Tomek Wytrębowicz <tomalecpub@gmail.com>
2018-08-17 09:49:56 +0200

    3.2.1

--- src/UniformDocs/Properties/AssemblyInfo.cs
+++ src/UniformDocs/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using Starcounter.Internal;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.2.0")]
-[assembly: AssemblyFileVersion("3.2.0")]
+[assembly: AssemblyVersion("3.2.1")]
+[assembly: AssemblyFileVersion("3.2.1")]
 
 // Assures the current assembly has a reference to the Starcounter
 // assembly. A reference to Starcounter is currently required for
